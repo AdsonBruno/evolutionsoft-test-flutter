1dfb9a12d3075ed1b9fe68854f7954cce078dc78
AdsonBruno <adsonbruno2@gmail.com>
2022-10-26 12:56:07 -0300

    feat: ui/themes/app_colors.dart and app_text_styles.dart

---
+++ lib/ui/themes/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF3B82F6);
+  static const secondary = Color(0xFFD1D1D1);
+  static const backgroundButton = Color(0xFFC9C9C9);
+  static const backgroundApp = Color(0xFFF5F5F5);
+  static const white = Color(0xFFFFFFFF);
+  static const title = Color(0xFF464646);
+  static const description = Color(0xFFA1A5B7);
+  static const red = Color(0XFFEB2E2E);
+  static const grey = Color(0xFF8C8C8C);
+}
