faa2a083623540d29f7be4929f448c149cade87d
AdsonBruno <adsonbruno2@gmail.com>
2022-10-28 15:57:27 -0300

    feat: app_widget - app settings-> screens routes, app theme

---
+++ lib/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:evolutionsoft_test_flutter/ui/modules/login/login_page.dart';
+import 'package:evolutionsoft_test_flutter/ui/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EvolutionMarket',
+      theme: ThemeData(
+        primaryColor: AppColors.backgroundApp,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+      },
+    );
+  }
+}
