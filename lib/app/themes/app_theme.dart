bb05fb9587913d5e1dd66bcd9ce40200250a6226
AdsonBruno <adsonbruno2@gmail.com>
2022-10-29 15:45:22 -0300

    feat: add ThemeData ao arquivo app_theme

---
+++ lib/app/themes/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:evolutionsoft_test_flutter/app/themes/app_colors.dart';
+
+final ThemeData appThemeData = ThemeData(
+  primaryColor: AppColors.backgroundApp,
+);
