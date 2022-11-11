31882105b152860b89c5dd7b2d1bdbe69c060174
AdsonBruno <adsonbruno2@gmail.com>
2022-11-11 14:59:25 -0300

    feat: add routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,22 +1,22 @@
-import 'package:evolutionsoft_test_flutter/app/modules/login/login_page.dart';
+import 'package:evolutionsoft_test_flutter/app/routes/app_pages.dart';
+import 'package:evolutionsoft_test_flutter/app/routes/app_routes.dart';
 import 'package:evolutionsoft_test_flutter/app/themes/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'EvolutionMarket',
       theme: ThemeData(
         primaryColor: AppColors.backgroundApp,
       ),
+      initialRoute: Routes.login,
+      getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-      },
     );
   }
 }
