78911c9a711ffe0774f9ee82f7b2ded81f6fc1c7
AdsonBruno <adsonbruno2@gmail.com>
2022-11-11 14:47:32 -0300

    feat: add route settings for screens

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,16 @@
+import 'package:evolutionsoft_test_flutter/app/modules/login/login_binding.dart';
+import 'package:evolutionsoft_test_flutter/app/modules/login/login_page.dart';
+import 'package:evolutionsoft_test_flutter/app/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static const login = Routes.login;
+
+  static final routes = [
+    GetPage(
+      name: Routes.login,
+      page: () => const LoginPage(), //InitialView(),
+      binding: LoginBinding(), //InitialBinding(),
+    )
+  ];
+}
