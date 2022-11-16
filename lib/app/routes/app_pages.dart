32b5c3a0e8bf75e5ffa78bc67e0211d09aad784f
AdsonBruno <adsonbruno2@gmail.com>
2022-11-16 09:26:16 -0300

    feat: add home routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,3 +1,5 @@
+import 'package:evolutionsoft_test_flutter/app/modules/home/home_binding.dart';
+import 'package:evolutionsoft_test_flutter/app/modules/home/home_page.dart';
 import 'package:evolutionsoft_test_flutter/app/modules/login/login_binding.dart';
 import 'package:evolutionsoft_test_flutter/app/modules/login/login_page.dart';
 import 'package:evolutionsoft_test_flutter/app/routes/app_routes.dart';
@@ -11,6 +13,11 @@ class AppPages {
       name: Routes.login,
       page: () => const LoginPage(), //InitialView(),
       binding: LoginBinding(), //InitialBinding(),
-    )
+    ),
+    GetPage(
+      name: Routes.home,
+      page: () => const HomePage(),
+      binding: HomeBinding(),
+    ),
   ];
 }
