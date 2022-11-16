20dc4dfca6e25d8389688f15cb8d97d5eeeb2f47
AdsonBruno <adsonbruno2@gmail.com>
2022-11-16 09:18:49 -0300

    feat: saves data to device with GetStorage

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:evolutionsoft_test_flutter/app/global/widgets/product_card_widget.dart';
+import 'package:evolutionsoft_test_flutter/app/modules/home/home_controller.dart';
+import 'package:evolutionsoft_test_flutter/app/themes/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends GetView<HomeController> {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: Get.mediaQuery.size.width,
+              height: Get.mediaQuery.size.height * 0.25,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/upper.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 24, right: 14),
+                      child: Image.asset(
+                        'assets/images/supermarket-car-with-white-background.png',
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 15),
+                      width: 125,
+                      height: 100,
+                      child: RichText(
+                        text: TextSpan(
+                            text: 'Confira nossos',
+                            style: TextStyles.textSize18White,
+                            children: [
+                              TextSpan(
+                                text: 'produtos',
+                                style: TextStyles.textSize24White,
+                              )
+                            ]),
+                      ),
+                    ),
+                    //
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            // GridView.count(crossAxisCount: 1, children: [
+            //   ProductCardWidget(
+            //     cardTitle: 'Alface',
+            //     cardDescription:
+            //         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+            //     onTap: () {},
+            //   ),
+            // ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/app/modules/login/login_controller.dart
+++ lib/app/modules/login/login_controller.dart
@@ -1,11 +1,15 @@
 import 'package:evolutionsoft_test_flutter/app/data/model/auth_model.dart';
 import 'package:evolutionsoft_test_flutter/app/data/repository/auth_repository.dart';
+import 'package:evolutionsoft_test_flutter/app/routes/app_routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+final box = GetStorage('evolutionsoft');
 
 class LoginController extends GetxController {
-  final repository = Get.find<AuthRepository>();
   AuthModel? authModel;
+  final repository = Get.find<AuthRepository>();
   final formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -20,6 +24,8 @@ class LoginController extends GetxController {
           .login(emailController.text, passwordController.text)
           .then((authModel) {
         loading.value = false;
+        box.write('authModel', authModel);
+        Get.offAllNamed(Routes.home);
       }).catchError((error) {
         loading.value = false;
       });
